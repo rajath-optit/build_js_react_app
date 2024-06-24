21a1b6e974c1f6f0845d8fe2a1b44e894fd1e289
bharathkoptit <128471256+bharathkoptit@users.noreply.github.com>
2024-06-25 02:38:24 +0530

    Update sonarCloudAnalysis_reactjs.groovy

--- CICD/Jenkins/shared-libraries/vars/common/code-analysis/sonarqube-util/sonarCloudAnalysis_reactjs.groovy
+++ CICD/Jenkins/shared-libraries/vars/common/code-analysis/sonarqube-util/sonarCloudAnalysis_reactjs.groovy
@@ -1,36 +1,11 @@
-import com.cloudbees.plugins.credentials.CredentialsProvider
-import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
-
-def performSonarCloudAnalysisforReactjs(String projectKey, String organization, String sourcesDir, String sonarCloudTokenId) {
-    def scannerHome = tool 'mysonar' // Use the name of your SonarQube installation
-    println "Using SonarQube scanner from: ${scannerHome}"
-    
-    // Retrieve SonarCloud token from Jenkins credentials
-    def credentials = CredentialsProvider.findCredentialById(sonarCloudTokenId, StandardUsernamePasswordCredentials.class, Jenkins.instance, [])
-    if (credentials == null) {
-        error "Could not find credentials with ID: ${sonarCloudTokenId}"
-        return
+def scannerHome = tool name: 'SonarQubeScanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+    withSonarQubeEnv('SonarQube') {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=my-react-project \
+            -Dsonar.sources=src \
+            -Dsonar.host.url=${env.SONAR_HOST_URL} \
+            -Dsonar.login=${env.SONAR_LOGIN}
+        """
     }
-    def sonarCloudToken = credentials.password
-
-    // Debug output
-    println "Project Key: ${projectKey}"
-    println "Organization: ${organization}"
-    println "Sources Directory: ${sourcesDir}"
-    println "SonarCloud Token: ${sonarCloudToken}"
-
-    // Run the SonarCloud analysis
-    withSonarQubeEnv('SonarCloud') {
-        def scannerCmd = "${scannerHome}/bin/sonar-scanner " +
-                         "-Dsonar.projectKey=${projectKey} " +
-                         "-Dsonar.organization=${organization} " +
-                         "-Dsonar.sources=${sourcesDir} " +
-                         "-Dsonar.host.url=https://sonarcloud.io " +
-                         "-Dsonar.login=${sonarCloudToken} " +
-                         "-X"
-
-        sh(script: scannerCmd, returnStatus: true) // Execute the scanner command and capture return status
-    }
-}
-
 return this
