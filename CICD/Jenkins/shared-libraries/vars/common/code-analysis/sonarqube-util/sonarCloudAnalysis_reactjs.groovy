1852e64dcab31edea2a41deedfcf1392ccc7a632
bharathkoptit <128471256+bharathkoptit@users.noreply.github.com>
2024-06-25 01:52:16 +0530

    Update sonarCloudAnalysis_reactjs.groovy

--- CICD/Jenkins/shared-libraries/vars/common/code-analysis/sonarqube-util/sonarCloudAnalysis_reactjs.groovy
+++ CICD/Jenkins/shared-libraries/vars/common/code-analysis/sonarqube-util/sonarCloudAnalysis_reactjs.groovy
@@ -1,21 +1,20 @@
-def performSonarCloudAnalysisforReactjs(String projectKey, String organization, String sourcesDir, String sonarCloudToken) {
- 
-  def scannerHome = tool 'mysonar' // Use the name of your SonarQube installation
-  println "Using SonarQube scanner from: ${scannerHome}"
-  def sonarCloudToken = credentials(sonarCloudTokenId)
+def performSonarCloudAnalysisforReactjs(String projectKey, String organization, String sourcesDir, String sonarCloudTokenId) {
+    def scannerHome = tool 'mysonar' // Use the name of your SonarQube installation
+    println "Using SonarQube scanner from: ${scannerHome}"
+    def sonarCloudToken = credentials(sonarCloudTokenId)
 
-  // Run the SonarCloud analysis
-  withSonarQubeEnv('SonarCloud') {
-    sh """
-         /var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/mysonar/bin/sonar-scanner \
-        -Dsonar.projectKey=${projectKey} \
-        -Dsonar.organization=${organization} \
-        -Dsonar.sources=${sourcesDir} \
-        -Dsonar.host.url=https://sonarcloud.io \
-        -Dsonar.login=${sonarCloudToken} \
-        -X
-    """
-  }
+    // Run the SonarCloud analysis
+    withSonarQubeEnv('SonarCloud') {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.organization=${organization} \
+            -Dsonar.sources=${sourcesDir} \
+            -Dsonar.host.url=https://sonarcloud.io \
+            -Dsonar.login=${sonarCloudToken} \
+            -X
+        """
+    }
 }
 
 return this
