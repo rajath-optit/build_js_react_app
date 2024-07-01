c625b4ed6903d66e03b41c528dd51d2fc0cac186
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-01 17:55:47 +0530

    Update ansibleWithVar.groovy

--- Jenkins/shared-libraries/vars/common/ansible/ansibleWithVar.groovy
+++ Jenkins/shared-libraries/vars/common/ansible/ansibleWithVar.groovy
@@ -1,5 +1,5 @@
 def playbook(inventoryFile, playBook, key1, value1, key2, value2) {
-     sh "ansible-playbook -i ${inventoryFile} ${playBook} --extra-vars "${key1}=${value1} ${key2}=${value2}" "
+     sh "ansible-playbook -i ${inventoryFile} ${playBook} --extra-vars "'${key1}'='${value1}' '${key2}'='${value2}'""
     //sh "ansible-playbook -i ${inventoryFile} ${playBook} --extra-vars "${key1}=${value1} ${key2}=${value2}" --vault-password-file /var/lib/jenkins/secrets/vault-pass
 }
 
