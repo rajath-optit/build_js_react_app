be04715412cd59088189ce26b82364c61ead081d
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-08 13:37:46 +0530

    Create DeployWithNode.groovy

---
+++ Jenkins/shared-libraries/vars/common/DeployManual/DeployWithNode.groovy
@@ -0,0 +1,12 @@
+def copyRepoToRemote(credentialID, remoteHost) {
+     withCredentials([sshUserPrivateKey(credentialsId: '${credentialID}', keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
+                        sh "scp -r -i $SSH_PRIVATE_KEY ${env.workspace}/* $SSH_USERNAME@${remoteHost}:/tmp/"
+     }
+}
+
+def deployWithNode(credentialID, remoteHost) {
+     withCredentials([sshUserPrivateKey(credentialsId: "${credentialID}", keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
+                        sh "ssh -i $SSH_PRIVATE_KEY $SSH_USERNAME@${remoteHost} 'cd /tmp/ ; npm install ; npm run build ; npm start '"
+     }
+}
+return this
