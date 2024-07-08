0fc9b5f7c211f4aafcf949446536a3c65c7562d8
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-08 17:13:35 +0530

    Create Jenkinsfile_war_gradle.groovy

---
+++ Jenkins/pipeline-templates/templates/ManualDeploy/Jenkinsfile_war_gradle.groovy
@@ -0,0 +1,63 @@
+@Library('shared-libraries') _
+
+tools {
+        // Install the Gradle version configured as "gradle" and add it to the path.
+        gradle "gradle"
+}
+
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'GIT_URL', description: 'Git repository URL contains Source Code')
+        string(name: 'GIT_BRANCH_NAME', description: 'Git Branch name')
+        credentials(name: 'GIT_CREDENTIAL', description: 'Jenkins credential for Git repo', required: true)
+        string(name: 'REMOTE_HOST', description: 'Remote host IP')
+        credentials(name: 'REMOTE_HOST_CREDENTIAL', description: 'Jenkins credential for connecting remote host', required: true)
+    }
+
+    stages {
+        stage('Load Scripts') {
+            steps {
+                script {
+                    // Define baseDir for Jenkins shared libraries
+                    def baseDir = "${env.WORKSPACE}/Jenkins/shared-libraries"
+
+                    // Load scripts using dynamically constructed paths
+                    def loadScript = { type, technology, scriptName ->
+                        def scriptPath = "${baseDir}/${type}/${technology}/${scriptName}.groovy"
+                        return load(scriptPath)
+                    }
+
+                    // Load scripts from 'vars' directory
+                    gitCheckoutScript = loadScript('vars/common/scm-util', 'git', 'GitCheckout')
+                    gradleDeployScript = loadScript('vars/common', 'ManualDeploy', 'DeployWithGradle')
+                }
+            }
+        }
+
+        stage('Git Checkout') {
+            steps {
+                script {
+                    gitCheckoutScript.gitCheckout(params.GIT_BRANCH_NAME, params.GIT_URL, params.GIT_CREDENTIAL)
+                }
+            }
+        }
+
+       stage('Package') {
+            steps {
+               script {
+                  gradleDeployScript.deployWithGradle()
+            }
+          }
+       }
+      
+       stage('Deploy Artifact in Tomcat') {
+            steps {
+               script {
+                  gradleDeployScript.deployWarToTomcat(params.REMOTE_HOST_CREDENTIAL, params.REMOTE_HOST)
+            }
+          }
+       }
+    }
+}
