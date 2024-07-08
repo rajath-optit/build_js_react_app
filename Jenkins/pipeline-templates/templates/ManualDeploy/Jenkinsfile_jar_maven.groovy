4d207b9af5e61f6302aceeb993d61ca343e1c0d2
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-08 16:23:22 +0530

    Create Jenkinsfile_jar_maven.groovy

---
+++ Jenkins/pipeline-templates/templates/ManualDeploy/Jenkinsfile_jar_maven.groovy
@@ -0,0 +1,71 @@
+@Library('shared-libraries') _
+
+tools {
+        // Install the Maven version configured as "maven" and add it to the path.
+        maven "maven"
+}
+
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'GIT_URL', description: 'Git repository URL contains Source Code')
+        string(name: 'GIT_BRANCH_NAME', description: 'Git Branch name')
+        credentials(name: 'GIT_CREDENTIAL', description: 'Jenkins credential for Git repo', required: true)
+        string(name: 'REMOTE_HOST', description: 'Remote host IP')
+        credentials(name: 'REMOTE_HOST_CREDENTIAL', description: 'Jenkins credential for connecting remote host', required: true)
+    }
+
+    stages {
+        stage('Load Scripts') {
+            steps {
+                script {
+                    // Define baseDir for Jenkins shared libraries
+                    def baseDir = "${env.WORKSPACE}/Jenkins/shared-libraries"
+
+                    // Load scripts using dynamically constructed paths
+                    def loadScript = { type, technology, scriptName ->
+                        def scriptPath = "${baseDir}/${type}/${technology}/${scriptName}.groovy"
+                        return load(scriptPath)
+                    }
+
+                    // Load scripts from 'vars' directory
+                    gitCheckoutScript = loadScript('vars/common/scm-util', 'git', 'GitCheckout')
+                    mavenDeployScript = loadScript('vars/common', 'ManualDeploy', 'DeployWithMaven')
+                }
+            }
+        }
+
+        stage('Git Checkout') {
+            steps {
+                script {
+                    gitCheckoutScript.gitCheckout(params.GIT_BRANCH_NAME, params.GIT_URL, params.GIT_CREDENTIAL)
+                }
+            }
+        }
+
+       stage('Package') {
+            steps {
+               script {
+                  mavenDeployScript.deployWithMaven()
+            }
+          }
+       }
+
+      stage('Copy Artifact to Remote') {
+            steps {
+               script {
+                  mavenDeployScript.copyJarToRemote(params.REMOTE_HOST_CREDENTIAL, params.REMOTE_HOST)
+            }
+          }
+       }
+      
+       stage('Deploy Artifact in Remote') {
+            steps {
+               script {
+                  mavenDeployScript.deployJar(params.REMOTE_HOST_CREDENTIAL, params.REMOTE_HOST)
+            }
+          }
+       }
+    }
+}
