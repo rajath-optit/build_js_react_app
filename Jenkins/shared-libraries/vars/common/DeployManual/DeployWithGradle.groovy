9ab7d275e2a784381fc5e4f28087f928097138f0
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-08 15:18:34 +0530

    Create DeployWithGradle.groovy

---
+++ Jenkins/shared-libraries/vars/common/DeployManual/DeployWithGradle.groovy
@@ -0,0 +1,34 @@
+def deployWithMaven() {
+    sh "gradle clean build"
+}
+
+def copyJarToRemote(credentialID, remoteHost) {
+  
+    withCredentials([sshUserPrivateKey(credentialsId: '${credentialID}', keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
+                        sh "scp -i $SSH_PRIVATE_KEY build/libs/*.jar $SSH_USERNAME@${remoteHost}:/root/"
+    }
+}
+
+def copyWarToRemote(credentialID, remoteHost) {
+  
+    withCredentials([sshUserPrivateKey(credentialsId: '${credentialID}', keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
+                        sh "scp -i $SSH_PRIVATE_KEY build/libs/*.war $SSH_USERNAME@${remoteHost}:/root/"
+    }
+}
+
+def deployWithJar(credentialID, remoteHost) {
+  
+   withCredentials([sshUserPrivateKey(credentialsId: "${credentialID}", keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
+                        sh "ssh -i $SSH_PRIVATE_KEY $SSH_USERNAME@${remoteHost} 'java -jar /root/*.jar'"
+    }
+}
+
+def deployWarToTomcat(credentialID, remoteHost) {
+  
+    withCredentials([sshUserPrivateKey(credentialsId: '${credentialID}', keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
+                        sh "scp -i $SSH_PRIVATE_KEY build/libs/*.war $SSH_USERNAME@${remoteHost}:/opt/tomcat/webapps/"
+                        sh "ssh -i $SSH_PRIVATE_KEY $SSH_USERNAME@${remoteHost} 'sudo systemctl restart tomcat'"
+    }
+}
+
+return this
