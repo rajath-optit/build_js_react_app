3d9612889a3aeede21a26fc836a10055c218cdd8
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-26 15:54:03 +0530

    Update DeployWithNodeJs.groovy

--- Jenkins/shared-libraries/vars/common/ManualDeploy/DeployWithNodeJs.groovy
+++ Jenkins/shared-libraries/vars/common/ManualDeploy/DeployWithNodeJs.groovy
@@ -1,12 +1,12 @@
-def copyRepoToRemote(credentialID, remoteHost) {
+def copyRepoToRemote(credentialID, remoteHost, remotePath) {
      withCredentials([sshUserPrivateKey(credentialsId: "${credentialID}", keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
-                        sh "scp -r -i $SSH_PRIVATE_KEY ${env.workspace}/* $SSH_USERNAME@${remoteHost}:/tmp/"
+                        sh "scp -r -i $SSH_PRIVATE_KEY ${env.workspace}/* $SSH_USERNAME@${remoteHost}:${remotePath}"
      }
 }
 
-def deployWithNode(credentialID, remoteHost) {
+def deployWithNode(credentialID, remoteHost, remotePath) {
      withCredentials([sshUserPrivateKey(credentialsId: "${credentialID}", keyFileVariable: 'SSH_PRIVATE_KEY', usernameVariable: 'SSH_USERNAME')]) {
-                        sh "ssh -i $SSH_PRIVATE_KEY $SSH_USERNAME@${remoteHost} 'cd /tmp/ ; npm install ; npm run build ; npm start '"
+                        sh "ssh -i $SSH_PRIVATE_KEY $SSH_USERNAME@${remoteHost} 'cd ${remotePath} ; npm install ; npm run build ; npm start '"
      }
 }
 return this
