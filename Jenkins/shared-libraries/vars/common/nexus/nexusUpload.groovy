ab9b345907a4b338090f9b31aab42e2053d76d9e
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-31 16:56:29 +0530

    Create nexusUpload.groovy

---
+++ Jenkins/shared-libraries/vars/common/nexus/nexusUpload.groovy
@@ -0,0 +1,7 @@
+def nexus(artifactId, artifactPath, artifactType, nexusCredentialID, nexusGroupID, nexusURL, nexusRepository, nexusVersion) {
+  
+               nexusArtifactUploader artifacts: [[artifactId: ${artifactId}, classifier: '', file: ${artifactPath}, type: ${artifactType}]], credentialsId: ${nexusCredentialID}, groupId: ${nexusGroupID}, nexusUrl: ${nexusURL}, nexusVersion: 'nexus3', protocol: 'http', repository: ${nexusRepository}, version: ${nexusVersion}
+            }
+}
+
+return this
