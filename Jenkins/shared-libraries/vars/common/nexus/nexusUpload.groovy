08cd55965f633d91c320653ff7210b1f6c917408
raj-Optit <168013366+raj-Optit@users.noreply.github.com>
2024-07-31 18:26:20 +0530

    Update nexusUpload.groovy

--- Jenkins/shared-libraries/vars/common/nexus/nexusUpload.groovy
+++ Jenkins/shared-libraries/vars/common/nexus/nexusUpload.groovy
@@ -1,4 +1,4 @@
-def nexusUpload() {
+def nexusuploads() {
   
                nexusArtifactUploader artifacts: [[artifactId: params.ARTIFACT_ID, classifier: '', file: params.ARTIFACT_PATH, type: params.ARTIFACT_TYPE]], credentialsId: params.NEXUS_CREDENTIAL_ID, groupId: params.NEXUS_GROUP_ID, nexusUrl: params.NEXUS_URL, nexusVersion: 'nexus3', protocol: 'http', repository: params.NEXUS_REPOSITORY, version: version
     
